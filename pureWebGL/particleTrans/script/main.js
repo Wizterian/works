39b80d7c65b5d851edbb658a5d670a11b6e6d0c4
makoto <>
2022-12-31 23:04:29 +0900

    cube coordinate

--- pureWebGL/particleTrans/script/main.js
+++ pureWebGL/particleTrans/script/main.js
@@ -36,8 +36,8 @@ class App {
     this.startTime = 0;
     this.currentTime = 0;
     this.render = this.render.bind(this);
-    this.ambientLight = 0.0;
-    this.lightVector = new Float32Array([0.0, 0.0, 1.0]);
+    // this.ambientLight = 0.0;
+    // this.lightVector = new Float32Array([0.0, 0.0, 1.0]);
   }
 
   init() {
@@ -113,9 +113,19 @@ class App {
   }
 
   setupGeometry() {
+    /**
+    ■立方体の体積
+    立方体の1辺の長さをaとすると，立方体の体積は
+    　a×a×a
+    ■球の体積
+    半径がrの球の体積は，
+    　r×r×r×π×4÷3
+    */
+
+    // Ball Geometry
     this.ballGeo = WebGLGeometry.sphere(
-      8, 8,
-      1 / 100,
+      6, 6,
+      1 / 80,
       [1, 1, 0, 1]
     );
     this.ballVBO = [
@@ -125,34 +135,30 @@ class App {
     ];
     this.ballIBO = WebGLUtility.createIBO(this.gl, this.ballGeo.index);
 
-    // Position & Color
+    // Ball Position & Color
     this.ballPosStep = 3;
     this.ballColStep = 4;
-    // Ball Position
-    this.ballNumCircle = 25; // 1周のパーティクル数
-    this.ballNumSphere = this.ballNumCircle * this.ballNumCircle;
-    const BALL_RADIUS = 1;
-    // geometry座標はfloat32Array（多分draw pointは普通の配列可）
+    // 極に集中してしまう
+    // https://techblog.kayac.com/how-to-distribute-points-randomly-using-high-school-math
+    this.ballCount = 3000;
+    const BALL_RADIUS = 1.25;
     this.ballTempPos = [];
     this.ballTempCol = [];
-    // this.ballColors = new Float32Array(this.ballNumCircle * this.ballColStep);
-    for (let i = 0; i < this.ballNumCircle; i += 1) {
-      const iRad = (i / this.ballNumCircle) * Math.PI * 2;
+    for (let i = 0; i < this.ballCount; i += 1) {
+      const iRad = Math.random() * Math.PI * 2;
+      const jRad = Math.random() * Math.PI;
       const x = Math.sin(iRad);
       const z = Math.cos(iRad);
-      for (let j = 0; j < this.ballNumCircle;  j += 1) {
-        const jRad = j / this.ballNumCircle * Math.PI;
-        const radius = Math.sin(jRad);
-        const y = Math.cos(jRad);
-        // position
-        this.ballTempPos.push(
-          x * BALL_RADIUS * radius,
-          y * BALL_RADIUS,
-          z * BALL_RADIUS * radius
-        );
-        this.ballTempCol.push(0.5, i / this.ballNumCircle, j / this.ballNumCircle, 1.0);
-      }
+      const radius = Math.sin(jRad);
+      const y = Math.cos(jRad);
+
+      this.ballTempPos.push(
+        x * BALL_RADIUS * radius,
+        y * BALL_RADIUS,
+        z * BALL_RADIUS * radius
+      );
     }
+    // Geo座標はfloat32Array（draw pointは多分配列可）
     this.ballPositions = new Float32Array(this.ballTempPos);
     this.ballColors = new Float32Array(this.ballTempCol);
 
@@ -164,6 +170,48 @@ class App {
     //   this.ballColors[i * this.ballColorStep + 1] = tmpRgb.g;
     //   this.ballColors[i * this.ballColorStep + 2] = tmpRgb.b;
     // }
+
+    // Cube Geometry
+    this.cubeGeo = WebGLGeometry.cube(1 / 40, [1, 0, 1, 1]);
+    this.cubeVBO = [
+      WebGLUtility.createVBO(this.gl, this.cubeGeo.position),
+      WebGLUtility.createVBO(this.gl, this.cubeGeo.normal),
+      WebGLUtility.createVBO(this.gl, this.cubeGeo.color),
+    ];
+    this.cubeIBO = WebGLUtility.createIBO(this.gl, this.cubeGeo.index);
+
+    // Cube Position & Color
+    this.cubePosStep = 3;
+    this.cubeColStep = 4;
+
+    this.cubeCount = this.ballCount;
+    const CUBE_EDGE = 1;
+    this.cubeTempPos = [];
+    this.cubeTempCol = [];
+    for (let i = 0; i < this.cubeCount; i += 1) {
+      const axis = Math.floor(Math.random() * 3);
+      let x, y, z;
+      if(axis === 0) {
+        x = Math.random() * 2 - 1;
+        y = Math.random() * 2 - 1;
+        z = Math.random() >= .5 ? -1 : 1;
+      } else if(axis === 1) {
+        x = Math.random() * 2 - 1;
+        y = Math.random() >= .5 ? -1 : 1;
+        z = Math.random() * 2 - 1;
+      } else if(axis === 2) {
+        x = Math.random() >= .5 ? -1 : 1;
+        y = Math.random() * 2 - 1;
+        z = Math.random() * 2 - 1;
+      }
+      this.cubeTempPos.push(
+        x * CUBE_EDGE,
+        y * CUBE_EDGE,
+        z * CUBE_EDGE
+      );
+    }
+    this.cubePositions = new Float32Array(this.cubeTempPos);
+    this.cubeColors = new Float32Array(this.cubeTempCol);
   }
 
   setupLocation() {
@@ -177,8 +225,8 @@ class App {
     this.standardUniLocation = {
       mvpMatrix: gl.getUniformLocation(this.standardProgram, 'mvpMatrix'), // MVP 行列
       normalMatrix: gl.getUniformLocation(this.standardProgram, 'normalMatrix'), // 法線変換行列
-      ambientLight: gl.getUniformLocation(this.standardProgram, 'ambientLight'), // 環境光
-      lightVector: gl.getUniformLocation(this.standardProgram, 'lightVector'), // ライトベクトル
+      // ambientLight: gl.getUniformLocation(this.standardProgram, 'ambientLight'), // 環境光
+      // lightVector: gl.getUniformLocation(this.standardProgram, 'lightVector'), // ライトベクトル
       // ballColor: gl.getUniformLocation(this.standardProgram, 'ballColor'), // 色
     };
   }
@@ -197,7 +245,7 @@ class App {
   start() {
     const gl = this.gl;
     this.startTime = Date.now();
-    this.isRender = true; // shader, geo, uniformできたら
+    this.isRender = true; // shader, geo, uniformできたらrenderループ開始
     this.render();
   }
 
@@ -217,54 +265,92 @@ class App {
       const p = m4.perspective(fovy, aspect, near, far);
       const vp = m4.multiply(p, v);
 
-      WebGLUtility.enableBuffer(gl, this.ballVBO, this.standardAttrLocation, this.standardAttrStride, this.ballIBO);
+      WebGLUtility.enableBuffer(gl, this.cubeVBO, this.standardAttrLocation, this.standardAttrStride, this.cubeIBO);
 
       const timeScale = 0.001;
       let m;
-      // return;
-      for(let i = 0; i < this.ballNumSphere; i++) {
+      for(let i = 0; i < this.cubeCount; i++) {
         m = m4.identity();
         // m = m4.rotate(m, this.currentTime * timeScale, v3.create(1.0, 1.0, 1.0));
-        // m = m4.rotate(m, this.currentTime * this.ballRotTimeScales[i], v3.create(
-        //   this.ballAxises[i * this.ballAxisStep + 0],
-        //   this.ballAxises[i * this.ballAxisStep + 1],
-        //   1.0 // 変数にすると不安定
-        // ));
-        // 個々の配置
         m = m4.translate(m, v3.create(
-          this.ballPositions[i * this.ballPosStep + 0],
-          this.ballPositions[i * this.ballPosStep + 1],
-          this.ballPositions[i * this.ballPosStep + 2],
+          this.cubePositions[i * this.cubePosStep + 0],
+          this.cubePositions[i * this.cubePosStep + 1],
+          this.cubePositions[i * this.cubePosStep + 2],
         ));
-        // m = m4.rotate(m, this.currentTime * this.cubeRotTimeScales[i], v3.create(
-        //   this.cubeAxises[i * this.cubeAxisStep + 0],
-        //   this.cubeAxises[i * this.cubeAxisStep + 1],
-        //   1.0
-        // ));
 
         const mvp = m4.multiply(vp, m);
         const normalMatrix = m4.transpose(m4.inverse(m));
 
         gl.uniformMatrix4fv(this.standardUniLocation.mvpMatrix, false, mvp);
         gl.uniformMatrix4fv(this.standardUniLocation.normalMatrix, false, normalMatrix);
-        gl.uniform3fv(
-          this.standardUniLocation.lightVector,
-          this.lightVector
-        );
-        gl.uniform4fv(this.standardUniLocation.ambientLight, [
-          this.ambientLight,
-          this.ambientLight,
-          this.ambientLight,
-          this.ambientLight
-        ]);
+        // gl.uniform3fv(
+        //   this.standardUniLocation.lightVector,
+        //   this.lightVector
+        // );
+        // gl.uniform4fv(this.standardUniLocation.ambientLight, [
+        //   this.ambientLight,
+        //   this.ambientLight,
+        //   this.ambientLight,
+        //   this.ambientLight
+        // ]);
         // gl.uniform4fv(this.standardUniLocation.ballColor, [
         //   this.ballColors[i * this.ballColorStep + 0],
         //   this.ballColors[i * this.ballColorStep + 1],
         //   this.ballColors[i * this.ballColorStep + 2],
         //   1.0
         // ]);
-        gl.drawElements(gl.TRIANGLES, this.ballGeo.index.length, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, this.cubeGeo.index.length, gl.UNSIGNED_SHORT, 0);
       }
     }
+
+    // // 通常描画
+    // {
+    //   this.setupStandardRendering();
+    //   const v = this.camera.update();
+    //   const fovy   = 45;
+    //   const aspect = window.innerWidth / window.innerHeight;
+    //   const near   = 0.1
+    //   const far    = 20.0;
+    //   const p = m4.perspective(fovy, aspect, near, far);
+    //   const vp = m4.multiply(p, v);
+
+    //   WebGLUtility.enableBuffer(gl, this.ballVBO, this.standardAttrLocation, this.standardAttrStride, this.ballIBO);
+
+    //   const timeScale = 0.001;
+    //   let m;
+    //   // return;
+    //   for(let i = 0; i < this.ballCount; i++) {
+    //     m = m4.identity();
+    //     // m = m4.rotate(m, this.currentTime * timeScale, v3.create(1.0, 1.0, 1.0));
+    //     m = m4.translate(m, v3.create(
+    //       this.ballPositions[i * this.ballPosStep + 0],
+    //       this.ballPositions[i * this.ballPosStep + 1],
+    //       this.ballPositions[i * this.ballPosStep + 2],
+    //     ));
+
+    //     const mvp = m4.multiply(vp, m);
+    //     const normalMatrix = m4.transpose(m4.inverse(m));
+
+    //     gl.uniformMatrix4fv(this.standardUniLocation.mvpMatrix, false, mvp);
+    //     gl.uniformMatrix4fv(this.standardUniLocation.normalMatrix, false, normalMatrix);
+    //     // gl.uniform3fv(
+    //     //   this.standardUniLocation.lightVector,
+    //     //   this.lightVector
+    //     // );
+    //     // gl.uniform4fv(this.standardUniLocation.ambientLight, [
+    //     //   this.ambientLight,
+    //     //   this.ambientLight,
+    //     //   this.ambientLight,
+    //     //   this.ambientLight
+    //     // ]);
+    //     // gl.uniform4fv(this.standardUniLocation.ballColor, [
+    //     //   this.ballColors[i * this.ballColorStep + 0],
+    //     //   this.ballColors[i * this.ballColorStep + 1],
+    //     //   this.ballColors[i * this.ballColorStep + 2],
+    //     //   1.0
+    //     // ]);
+    //     gl.drawElements(gl.TRIANGLES, this.ballGeo.index.length, gl.UNSIGNED_SHORT, 0);
+    //   }
+    // }
   }
 }
--- pureWebGL/particleTrans/shader/standard.vert
+++ pureWebGL/particleTrans/shader/standard.vert
@@ -5,24 +5,30 @@ attribute vec4 color;
 
 uniform mat4 mvpMatrix;
 uniform mat4 normalMatrix;
-uniform vec3 lightVector;
-uniform vec4 ambientLight;
+// uniform vec3 lightVector;
+// uniform vec4 ambientLight;
 // uniform int normalVisibility;
 
 varying vec4 vColor;
 
 void main(){
+  // to Escape Warning
+  normalMatrix;
+  normal;
 
-  vec3 n = (normalMatrix * vec4(normal, 0.0)).xyz;
-  // 変換した法線とライトベクトルで内積を取る @@@
+  // vec3 n = (normalMatrix * vec4(normal, 0.0)).xyz;
   /************************************
-  単位化した方向(dot)で光源をシミュレート（-1〜+1）
-  clampは範囲内に丸める関数
+  ・変換した法線とライトベクトルで内積を取る
+  ・単位化した方向(dot)で光源をシミュレート（-1〜+1）
+  ・clampは範囲内に丸める関数
   */
-  float d = clamp(dot(normalize(n), normalize(lightVector)), 0.5, 1.0);
+  // float d = clamp(dot(normalize(n), normalize(lightVector)), 0.5, 1.0); // no light vec
 
   // 内積の結果を頂点カラーのRGB成分に乗算する
-  vColor = vec4(color.rgb * d, color.a) + ambientLight;
+  // vColor = vec4(color.rgb * d, color.a) + ambientLight; // no ambient light
+
+  // new line
+  vColor = vec4(color.rgb, color.a);
 
   // 座標変換
   gl_Position = mvpMatrix * vec4(position, 1.0);
